03a24843f46996a7fccec94c7600fbc3bf4bf491
tan-xianglong <t.xianglong@gmail.com>
2022-03-30 00:57:56 +0800

    get data from Openweather

--- public/main.js
+++ public/main.js
@@ -50,7 +50,19 @@ const getPlaces = async () => {
   }
 };
 
-const getForecast = () => {};
+const getForecast = async () => {
+  const urlToFetch = `${weatherUrl}?q=${$input.val()}&APPID=${openWeatherKey}`;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+    }
+    return jsonResponse;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Render functions
 const renderPlaces = (places) => {
